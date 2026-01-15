e79d1801d952807d30ba9a88bfda5dc67751c6ad
homfunctor <thehundredvisions@proton.me>
2026-01-14 23:40:37 -0600

    really? it's the order of the headers? goddamn.

--- CvGameCoreDLLUtil/include/CustomModsGlobal.h
+++ CvGameCoreDLLUtil/include/CustomModsGlobal.h
@@ -7,7 +7,7 @@
 // Edit this to change the number of available major civs
 // Valid values are 2 to 62, 34 seems to be the accepted "improved" value
 // For the default value of 22, just comment the line out
-// #define MOD_GLOBAL_MAX_MAJOR_CIVS (34)
+#define MOD_GLOBAL_MAX_MAJOR_CIVS (34)
 // #define MOD_GLOBAL_MAX_MAJOR_CIVS (43)
 // #define MOD_GLOBAL_MAX_MAJOR_CIVS (62)
 
